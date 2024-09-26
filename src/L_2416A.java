a81e7f26717baccefbe1cecbf813179a12398722
Valixon <mr.valixon.1991@gmail.com>
2024-09-26 09:53:34 +0500

    hammasi

---
+++ src/L_2416A.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+public class L_2416A {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sumPrefixScores(new String[]{"abc", "ab", "bc", "b"})));
+        System.out.println(Arrays.toString(sumPrefixScores(new String[]{"abcd"})));
+        System.out.println(Arrays.toString(sumPrefixScores(new String[]{"hfjuiwcab", "hfjuiwfeqm", "hfjuif", "hfjuiwfeuv", "ammrchqrt", "qaoqjoqesy", "hfjuiwfohq"})));
+    }
+
+
+    public static int[] sumPrefixScores(String[] words) {
+        int n = words.length;
+        Map<String, Integer> map = new HashMap<>();
+        List<String>[] bori = new List[n];
+        for (int i = 0; i < n; i++) {
+            bori[i] = new ArrayList<>();
+        }
+        for (int k = 0; k < n; k++) {
+            String word = words[k];
+            String sum = "";
+            for (int i = 0; i < word.length(); i++) {
+                sum += "" + word.charAt(i);
+                bori[k].add(sum);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        int[] res = new int[words.length];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < bori[i].size(); j++) {
+                String sum = bori[i].get(j);
+                int x = map.get(sum);
+                res[i] += x;
+                if (x == 1) {
+                    res[i] += (bori[i].size() - j - 1);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int[] sumPrefixScores2(String[] words) {
+        int n = words.length;
+        int[] res = new int[n];
+        int[] qiymat = new int['z' + 1];
+        List<Long>[] bori = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            bori[i] = new ArrayList<Long>();
+        }
+        qiymat['a'] = 11;
+        qiymat['b'] = 13;
+        qiymat['c'] = 17;
+        qiymat['d'] = 19;
+        qiymat['e'] = 23;
+        qiymat['f'] = 29;
+        qiymat['g'] = 31;
+        qiymat['h'] = 37;
+        qiymat['i'] = 41;
+        qiymat['j'] = 43;
+        qiymat['k'] = 47;
+        qiymat['l'] = 53;
+        qiymat['m'] = 59;
+        qiymat['n'] = 61;
+        qiymat['o'] = 67;
+        qiymat['p'] = 71;
+        qiymat['q'] = 73;
+        qiymat['r'] = 79;
+        qiymat['s'] = 83;
+        qiymat['t'] = 89;
+        qiymat['u'] = 97;
+        qiymat['v'] = 101;
+        qiymat['w'] = 103;
+        qiymat['x'] = 107;
+        qiymat['y'] = 109;
+        qiymat['z'] = 113;
+        Map<Long, Integer> map = new TreeMap<>();
+        for (int k = 0; k < n; k++) {
+            String word = words[k];
+            long sum = 0;
+            for (int i = 0; i < word.length(); i++) {
+                sum = sum + (qiymat[word.charAt(i)] * ((i + 3001) * (i + 1001)));
+                bori[k].add(sum);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (Long sum : bori[i]) {
+                res[i] += map.get(sum);
+            }
+        }
+        return res;
+    }
+
+
+    private static int[] getTuplar() {
+        int[] a = new int[1001];
+        int start = 115;
+        for (int i = 0; i <= 1000; i++) {
+            while (!tubmi(start)) {
+                start++;
+            }
+            a[i] = start;
+        }
+        return a;
+    }
+
+    private static boolean tubmi(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+}
---
+++ src/L_440.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class L_440 {
+    public static void main(String[] args) {
+        System.out.println(findKthNumber(13, 2));
+        System.out.println(findKthNumber(1, 1));
+        System.out.println(findKthNumber(1, 1));
+        System.out.println(findKthNumber(100, 9));
+        System.out.println(findKthNumber(3564, 5));
+    }
+
+    public static int findKthNumber(int n, int k) {
+        if (n < 10) return k;
+        return findKthNumber2(n, k);
+    }
+
+    public static int findKthNumber2(int n, int k) {
+        if (n < 10) return k - 1;
+        int[] a = new int[10];
+        for (int i = 1; i <= 9; i++) {
+            a[i] = 1;
+            int y = i;
+            int p = 1;
+            while (y * 10 + (p * 10) - 1 <= n) {
+                p *= 10;
+                y *= 10;
+                a[i] += p;
+            }
+            System.out.println(n % (y)+" "+ p);
+            if (n % (y * 10) < p * 10) {
+                a[i] += (n % (y * 10) + 1);
+            }
+        }
+        System.out.println(n + " " + k + " " + Arrays.toString(a));
+        for (int i = 1; i <= 9; i++) {
+            if (a[i] <= k) {
+                k = k - a[i];
+            } else {
+
+            }
+
+        }
+        return 1;
+    }
+
+    public static List<Integer> lexicalOrder(int n) {
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = "" + (i + 1);
+        }
+        Arrays.sort(a);
+        List<Integer> res = new ArrayList<>();
+        for (String i : a)
+            res.add(Integer.parseInt(i));
+        return res;
+    }
+}
